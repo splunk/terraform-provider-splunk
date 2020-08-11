553551244eca0df314c6d8e2245e017bfb823b48
ajayaraman <ajayaraman@splunk.com>
2020-08-10 18:28:01 -0700

    Adding tcp cooked inputs

---
+++ client/inputs_tcp_cooked.go
@@ -0,0 +1,65 @@
+package client
+
+import (
+	"github.com/google/go-querystring/query"
+	"net/http"
+	"terraform-provider-splunk/client/models"
+)
+
+func (client *Client) CreateTCPCookedInput(name string, owner string, app string, inputsTCPCookedObject *models.InputsTCPCookedObject) error {
+	values, err := query.Values(inputsTCPCookedObject)
+	values.Add("name", name)
+	if err != nil {
+		return err
+	}
+
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "data", "inputs", "tcp", "cooked")
+	resp, err := client.Post(endpoint, values)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func (client *Client) ReadTCPCookedInput(name, owner, app string) (*http.Response, error) {
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "data", "inputs", "tcp", "cooked", name)
+	resp, err := client.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (client *Client) UpdateTCPCookedInput(name string, owner string, app string, inputsTCPCookedObject *models.InputsTCPCookedObject) error {
+	values, err := query.Values(&inputsTCPCookedObject)
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "data", "inputs", "tcp", "cooked", name)
+	resp, err := client.Post(endpoint, values)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return nil
+}
+
+func (client *Client) DeleteTCPCookedInput(name, owner, app string) (*http.Response, error) {
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "data", "inputs", "tcp", "cooked", name)
+	resp, err := client.Delete(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// services/data/inputs/tcp/cooked
+func (client *Client) ReadTCPCookedInputs() (*http.Response, error) {
+	endpoint := client.BuildSplunkURL(nil, "services", "data", "inputs", "tcp", "cooked")
+	resp, err := client.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
