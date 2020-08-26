8806a36486339d4021e2b799363ae8dd5b6a7958
Nicolas Stone <nstone@splunk.com>
2020-08-26 10:16:16 -0700

    Centralized name splitting

--- client/conf_stanza.go
+++ client/conf_stanza.go
@@ -4,7 +4,6 @@ import (
 	"github.com/google/go-querystring/query"
 	"net/http"
 	"terraform-provider-splunk/client/models"
-	"strings"
 )
 
 func (client *Client) CreateConfStanzaObject(name string, owner string, app string, confStanzaConfigObj *models.ConfStanzaObject) error {
@@ -13,13 +12,12 @@ func (client *Client) CreateConfStanzaObject(name string, owner string, app stri
 	for k, v := range confStanzaConfigObj.Variables {
 		values.Add(string(k), string(v))
 	}
-	split_name := strings.Split(name, "/")
-	conf_name := split_name[0]
-	stanza_name := split_name[1]
-	values.Add("name", stanza_name)
+
+	conf, stanza := client.SplitConfStanza(name)
+	values.Add("name", stanza)
 	values.Del("Variables")
 
-	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf_name)
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf)
 	resp, err := client.Post(endpoint, values)
 	if err != nil {
 		return err
@@ -30,11 +28,9 @@ func (client *Client) CreateConfStanzaObject(name string, owner string, app stri
 }
 
 func (client *Client) ReadConfStanzaObject(name, owner, app string) (*http.Response, error) {
-	split_name := strings.Split(name, "/")
-	conf_name := split_name[0]
-	stanza_name := split_name[1]
+	conf, stanza := client.SplitConfStanza(name)
 
-	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf_name, stanza_name)
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf, stanza)
 	resp, err := client.Get(endpoint)
 	if err != nil {
 		return nil, err
@@ -50,12 +46,10 @@ func (client *Client) UpdateConfStanzaObject(name string, owner string, app stri
 		values.Add(string(k), string(v))
 	}
 
-	split_name := strings.Split(name, "/")
-	conf_name := split_name[0]
-	stanza_name := split_name[1]
+	conf, stanza := client.SplitConfStanza(name)
 	values.Del("Variables")
 
-	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf_name, stanza_name)
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf, stanza)
 	resp, err := client.Post(endpoint, values)
 	if err != nil {
 		return err
@@ -66,11 +60,9 @@ func (client *Client) UpdateConfStanzaObject(name string, owner string, app stri
 }
 
 func (client *Client) DeleteConfStanzaObject(name, owner, app string) (*http.Response, error) {
-	split_name := strings.Split(name, "/")
-	conf_name := split_name[0]
-	stanza_name := split_name[1]
+	conf, stanza := client.SplitConfStanza(name)
 
-	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf_name, stanza_name)
+	endpoint := client.BuildSplunkURL(nil, "servicesNS", owner, app, "configs", "conf-" + conf, stanza)
 
 	resp, err := client.Delete(endpoint)
 	if err != nil {
@@ -81,10 +73,9 @@ func (client *Client) DeleteConfStanzaObject(name, owner, app string) (*http.Res
 }
 
 func (client *Client) ReadAllConfStanzaObject(name string) (*http.Response, error) {
-	split_name := strings.Split(name, "/")
-	conf_name := split_name[0]
+	conf, _ := client.SplitConfStanza(name)
 
-	endpoint := client.BuildSplunkURL(nil, "services", "configs", "conf-" + conf_name)
+	endpoint := client.BuildSplunkURL(nil, "services", "configs", "conf-" + conf)
 
 	resp, err := client.Get(endpoint)
 	if err != nil {
