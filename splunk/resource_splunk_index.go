9da05809353085056dd30e8e52bafdde40dc3d96
ajayaraman <ajayaraman@splunk.com>
2020-08-26 18:51:03 -0700

    Moving default params to computed

--- splunk/resource_splunk_index.go
+++ splunk/resource_splunk_index.go
@@ -3,6 +3,7 @@ package splunk
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"net/http"
 	"regexp"
@@ -15,7 +16,7 @@ func index() *schema.Resource {
 			"block_sign_size": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  0,
+				Computed: true,
 				Description: `Controls how many events make up a block for block signatures.
 				If this is set to 0, block signing is disabled for this index.
 				A recommended value is 100.`,
@@ -26,15 +27,15 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `Suggestion for the bucket rebuild process for the size of the time-series (tsidx) file to make.
 				Caution: This is an advanced parameter. Inappropriate use of this parameter causes splunkd to not start if rebuild is required. Do not set this parameter unless instructed by Splunk Support.
-				
+
 				Default value, auto, varies by the amount of physical RAM on the host
-				
+
 				- less than 2GB RAM = 67108864 (64MB) tsidx
 				- 2GB to 8GB RAM = 134217728 (128MB) tsidx
 				- more than 8GB RAM = 268435456 (256MB) tsidx
-				
+
 				Values other than "auto" must be 16MB-1GB. Highest legal value (of the numerical part) is 4294967295
-				
+
 				You can specify the value using a size suffix: "16777216" or "16MB" are equivalent.`,
 			},
 			"cold_path": {
@@ -50,12 +51,12 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `Destination path for the frozen archive. Use as an alternative to a coldToFrozenScript. Splunk software automatically puts frozen buckets in this directory.
 				Bucket freezing policy is as follows:
-				
+
 				- New style buckets (4.2 and on): removes all files but the rawdata
 				To thaw, run splunk rebuild <bucket dir> on the bucket, then move to the thawed directory
 				- Old style buckets (Pre-4.2): gzip all the .data and .tsidx files
 				To thaw, gunzip the zipped files and move the bucket into the thawed directory
-				
+
 				If both coldToFrozenDir and coldToFrozenScript are specified, coldToFrozenDir takes precedence`,
 			},
 			"cold_to_frozen_script": {
@@ -64,43 +65,43 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `Path to the archiving script.
 				If your script requires a program to run it (for example, python), specify the program followed by the path. The script must be in $SPLUNK_HOME/bin or one of its subdirectories.
-				
+
 				Splunk software ships with an example archiving script in $SPLUNK_HOME/bin called coldToFrozenExample.py. DO NOT use this example script directly. It uses a default path, and if modified in place any changes are overwritten on upgrade.
-				
+
 				It is best to copy the example script to a new file in bin and modify it for your system. Most importantly, change the default archive path to an existing directory that fits your needs.
-				
+
 				If your new script in bin/ is named myColdToFrozen.py, set this key to the following:
-				
+
 				coldToFrozenScript = "$SPLUNK_HOME/bin/python" "$SPLUNK_HOME/bin/myColdToFrozen.py"
-				
+
 				By default, the example script has two possible behaviors when archiving:
-				
+
 				- For buckets created from version 4.2 and on, it removes all files except for rawdata. To thaw: cd to the frozen bucket and type splunk rebuild ., then copy the bucket to thawed for that index. We recommend using the coldToFrozenDir parameter unless you need to perform a more advanced operation upon freezing buckets.
 				- For older-style buckets, we simply gzip all the .tsidx files. To thaw: cd to the frozen bucket and unzip the tsidx files, then copy the bucket to thawed for that index`,
 			},
 			"compress_rawdata": {
 				Type:        schema.TypeBool,
 				Optional:    true,
-				Default:     true,
+				Computed:    true,
 				Description: `This parameter is ignored. The splunkd process always compresses raw data.`,
 			},
 			"datatype": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				Default:     "event",
+				Computed:    true,
 				Description: `Valid values: (event | metric). Specifies the type of index.`,
 			},
 			"enable_online_bucket_repair": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				Default:  true,
+				Computed: true,
 				Description: `Enables asynchronous "online fsck" bucket repair, which runs concurrently with Splunk software.
 				When enabled, you do not have to wait until buckets are repaired to start the Splunk platform. However, you might observe a slight performance degratation.`,
 			},
 			"frozen_time_period_in_secs": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  188697600,
+				Computed: true,
 				Description: `Number of seconds after which indexed data rolls to frozen. Defaults to 188697600 (6 years).
 				Freezing data means it is removed from the index. If you need to archive your data, refer to coldToFrozenDir and coldToFrozenScript parameter documentation.`,
 			},
@@ -110,122 +111,122 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `An absolute path that contains the hot and warm buckets for the index.
 				Required. Splunk software does not start if an index lacks a valid homePath.
-				
+
 				Caution: The path must be readable and writable.`,
 			},
 			"max_bloom_backfill_bucket_age": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				Default:     "30d",
+				Computed:    true,
 				Description: `This parameter is ignored. The splunkd process always compresses raw data.`,
 			},
 			"max_concurrent_optimizes": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  6,
+				Computed: true,
 				Description: `The number of concurrent optimize processes that can run against a hot bucket.
 				This number should be increased if instructed by Splunk Support. Typically the default value should suffice.`,
 			},
 			"max_data_size": {
 				Type:     schema.TypeString,
 				Optional: true,
-				Default:  "auto",
+				Computed: true,
 				Description: `The maximum size in MB for a hot DB to reach before a roll to warm is triggered. Specifying "auto" or "auto_high_volume" causes Splunk software to autotune this parameter (recommended).Use "auto_high_volume" for high volume indexes (such as the main index); otherwise, use "auto". A "high volume index" would typically be considered one that gets over 10GB of data per day.
 				- "auto" sets the size to 750MB.
 				- "auto_high_volume" sets the size to 10GB on 64-bit, and 1GB on 32-bit systems.
-				
+
 				Although the maximum value you can set this is 1048576 MB, which corresponds to 1 TB, a reasonable number ranges anywhere from 100 - 50000. Any number outside this range should be approved by Splunk Support before proceeding.
-				
+
 				If you specify an invalid number or string, maxDataSize is auto-tuned.
-				
+
 				Note: The precise size of your warm buckets may vary from maxDataSize, due to post-processing and timing issues with the rolling policy.`,
 			},
 			"max_hot_buckets": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  3,
+				Computed: true,
 				Description: `Maximum hot buckets that can exist per index. Defaults to 3.
 				When maxHotBuckets is exceeded, Splunk software rolls the least recently used (LRU) hot bucket to warm. Both normal hot buckets and quarantined hot buckets count towards this total. This setting operates independently of maxHotIdleSecs, which can also cause hot buckets to roll.`,
 			},
 			"max_hot_idle_secs": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  0,
+				Computed: true,
 				Description: `Maximum life, in seconds, of a hot bucket. Defaults to 0.
 				If a hot bucket exceeds maxHotIdleSecs, Splunk software rolls it to warm. This setting operates independently of maxHotBuckets, which can also cause hot buckets to roll. A value of 0 turns off the idle check (equivalent to INFINITE idle time).`,
 			},
 			"max_hot_span_secs": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  7776000,
+				Computed: true,
 				Description: `Upper bound of target maximum timespan of hot/warm buckets in seconds. Defaults to 7776000 seconds (90 days).
 				Note: If you set this too small, you can get an explosion of hot/warm buckets in the filesystem. The system sets a lower bound implicitly for this parameter at 3600, but this is an advanced parameter that should be set with care and understanding of the characteristics of your data.`,
 			},
 			"max_mem_mb": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  5,
+				Computed: true,
 				Description: `The amount of memory, expressed in MB, to allocate for buffering a single tsidx file into memory before flushing to disk. Defaults to 5. The default is recommended for all environments.
 				IMPORTANT: Calculate this number carefully. Setting this number incorrectly may have adverse effects on your systems memory and/or splunkd stability/performance.`,
 			},
 			"max_meta_entries": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  1000000,
+				Computed: true,
 				Description: `Sets the maximum number of unique lines in .data files in a bucket, which may help to reduce memory consumption. If set to 0, this setting is ignored (it is treated as infinite).
 				If exceeded, a hot bucket is rolled to prevent further increase. If your buckets are rolling due to Strings.data hitting this limit, the culprit may be the punct field in your data. If you do not use punct, it may be best to simply disable this (see props.conf.spec in $SPLUNK_HOME/etc/system/README).
-				
+
 				There is a small time delta between when maximum is exceeded and bucket is rolled. This means a bucket may end up with epsilon more lines than specified, but this is not a major concern unless excess is significant.`,
 			},
 			"max_time_unreplicated_no_acks": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  300,
+				Computed: true,
 				Description: `Upper limit, in seconds, on how long an event can sit in raw slice. Applies only if replication is enabled for this index. Otherwise ignored.
 				If there are any acknowledged events sharing this raw slice, this paramater does not apply. In this case, maxTimeUnreplicatedWithAcks applies.
-				
+
 				Highest legal value is 2147483647. To disable this parameter, set to 0.
-				
+
 				Note: this is an advanced parameter. Understand the consequences before changing.`,
 			},
 			"max_time_unreplicated_with_acks": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  60,
+				Computed: true,
 				Description: `Upper limit, in seconds, on how long events can sit unacknowledged in a raw slice. Applies only if you have enabled acks on forwarders and have replication enabled (with clustering).
 				Note: This is an advanced parameter. Make sure you understand the settings on all forwarders before changing this. This number should not exceed ack timeout configured on any forwarder, and should actually be set to at most half of the minimum value of that timeout. You can find this setting in outputs.conf readTimeout setting under the tcpout stanza.
-				
+
 				To disable, set to 0, but this is NOT recommended. Highest legal value is 2147483647.`,
 			},
 			"max_total_data_size_mb": {
 				Type:        schema.TypeInt,
 				Optional:    true,
-				Default:     500000,
+				Computed:    true,
 				Description: `The maximum size of an index (in MB). If an index grows larger than the maximum size, the oldest data is frozen.`,
 			},
 			"max_warm_db_count": {
 				Type:        schema.TypeInt,
 				Optional:    true,
-				Default:     300,
+				Computed:    true,
 				Description: `The maximum number of warm buckets. If this number is exceeded, the warm bucket/s with the lowest value for their latest times is moved to cold.`,
 			},
 			"min_raw_file_sync_secs": {
 				Type:     schema.TypeString,
 				Optional: true,
-				Default:  "disable",
+				Computed: true,
 				Description: `Specify an integer (or "disable") for this parameter.
 				This parameter sets how frequently splunkd forces a filesystem sync while compressing journal slices.
-				
+
 				During this period, uncompressed slices are left on disk even after they are compressed. Then splunkd forces a filesystem sync of the compressed journal and removes the accumulated uncompressed files.
-				
+
 				If 0 is specified, splunkd forces a filesystem sync after every slice completes compressing. Specifying "disable" disables syncing entirely: uncompressed slices are removed as soon as compression is complete.
-				
+
 				Note: Some filesystems are very inefficient at performing sync operations, so only enable this if you are sure it is needed`,
 			},
 			"min_stream_group_queue_size": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  2000,
+				Computed: true,
 				Description: `Minimum size of the queue that stores events in memory before committing them to a tsidx file.
 				Caution: Do not set this value, except under advice from Splunk Support.`,
 			},
@@ -238,44 +239,44 @@ func index() *schema.Resource {
 			"partial_service_meta_period": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  0,
+				Computed: true,
 				Description: `Related to serviceMetaPeriod. If set, it enables metadata sync every <integer> seconds, but only for records where the sync can be done efficiently in-place, without requiring a full re-write of the metadata file. Records that require full re-write are be sync'ed at serviceMetaPeriod.
 				partialServiceMetaPeriod specifies, in seconds, how frequently it should sync. Zero means that this feature is turned off and serviceMetaPeriod is the only time when metadata sync happens.
-				
+
 				If the value of partialServiceMetaPeriod is greater than serviceMetaPeriod, this setting has no effect.
-				
+
 				By default it is turned off (zero).`,
 			},
 			"process_tracker_service_interval": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  1,
+				Computed: true,
 				Description: `Specifies, in seconds, how often the indexer checks the status of the child OS processes it launched to see if it can launch new processes for queued requests. Defaults to 15.
 				If set to 0, the indexer checks child process status every second.
-				
+
 				Highest legal value is 4294967295.`,
 			},
 			"quarantine_future_secs": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  2592000,
+				Computed: true,
 				Description: `Events with timestamp of quarantineFutureSecs newer than "now" are dropped into quarantine bucket. Defaults to 2592000 (30 days).
 				This is a mechanism to prevent main hot buckets from being polluted with fringe events.`,
 			},
 			"quarantine_past_secs": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  77760000,
+				Computed: true,
 				Description: `Events with timestamp of quarantinePastSecs older than "now" are dropped into quarantine bucket. Defaults to 77760000 (900 days).
 				This is a mechanism to prevent the main hot buckets from being polluted with fringe events.`,
 			},
 			"raw_chunk_size_bytes": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  131072,
+				Computed: true,
 				Description: `Target uncompressed size in bytes for individual raw slice in the rawdata journal of the index. Defaults to 131072 (128KB). 0 is not a valid value. If 0 is specified, rawChunkSizeBytes is set to the default value.
 				Note: rawChunkSizeBytes only specifies a target chunk size. The actual chunk size may be slightly larger by an amount proportional to an individual event size.
-				
+
 				WARNING: This is an advanced parameter. Only change it if you are instructed to do so by Splunk Support.`,
 			},
 			"rep_factor": {
@@ -284,26 +285,26 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `Index replication control. This parameter applies to only clustering slaves.
 				auto = Use the master index replication configuration value.
-				
+
 				0 = Turn off replication for this index.`,
 			},
 			"rotate_period_in_secs": {
 				Type:        schema.TypeInt,
 				Optional:    true,
-				Default:     60,
+				Computed:    true,
 				Description: `How frequently (in seconds) to check if a new hot bucket needs to be created. Also, how frequently to check if there are any warm/cold buckets that should be rolled/frozen.`,
 			},
 			"service_meta_period": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  25,
+				Computed: true,
 				Description: `Defines how frequently metadata is synced to disk, in seconds. Defaults to 25 (seconds).
 				You may want to set this to a higher value if the sum of your metadata file sizes is larger than many tens of megabytes, to avoid the hit on I/O in the indexing fast path.`,
 			},
 			"sync_meta": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				Default:  true,
+				Computed: true,
 				Description: `When true, a sync operation is called before file descriptor is closed on metadata file updates. This functionality improves integrity of metadata files, especially in regards to operating system crashes/machine failures.
 				Note: Do not change this parameter without the input of a Splunk Support.`,
 			},
@@ -313,13 +314,13 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `An absolute path that contains the thawed (resurrected) databases for the index.
 				Cannot be defined in terms of a volume definition.
-				
+
 				Required. Splunk software does not start if an index lacks a valid thawedPath.`,
 			},
 			"throttle_check_period": {
 				Type:     schema.TypeInt,
 				Optional: true,
-				Default:  15,
+				Computed: true,
 				Description: `Defines how frequently Splunk software checks for index throttling condition, in seconds. Defaults to 15 (seconds).
 				Note: Do not change this parameter without the input of Splunk Support.`,
 			},
@@ -329,9 +330,9 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `Location to store datamodel acceleration TSIDX data for this index. Restart splunkd after changing this parameter.
 				If specified, it must be defined in terms of a volume definition.
-				
+
 				Caution: Path must be writable.
-				
+
 				Default value: volume:_splunk_summaries/$_index_name/tstats`,
 			},
 			"warm_to_cold_script": {
@@ -340,7 +341,7 @@ func index() *schema.Resource {
 				Computed: true,
 				Description: `Path to a script to run when moving data from warm to cold.
 				This attribute is supported for backwards compatibility with Splunk software versions older than 4.0. Contact Splunk support if you need help configuring this setting.
-				
+
 				Caution: Migrating data across filesystems is now handled natively by splunkd. If you specify a script here, the script becomes responsible for moving the event data, and Splunk-native data migration is not used.`,
 			},
 			"acl": aclSchema(),
@@ -395,6 +396,10 @@ func indexRead(d *schema.ResourceData, meta interface{}) error {
 	entry, err := getIndexConfigByName(name, resp)
 	if err != nil {
 		return err
+	}
+
+	if entry == nil {
+		return errors.New(fmt.Sprintf("Unable to find resource: %v", name))
 	}
 
 	// Now we read the input configuration with proper owner and app
@@ -502,6 +507,10 @@ func indexRead(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	if err = d.Set("min_stream_group_queue_size", entry.Content.MinStreamGroupQueueSize); err != nil {
+		return err
+	}
+
+	if err = d.Set("name", d.Id()); err != nil {
 		return err
 	}
 
