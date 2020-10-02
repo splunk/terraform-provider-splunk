f56aa6f2c2e5d2662989510de9d720689abcf595
ajayaraman <ajayaraman@splunk.com>
2020-10-02 14:55:32 -0400

    remove skip docker var

--- splunk/resource_splunk_inputs_script_test.go
+++ splunk/resource_splunk_inputs_script_test.go
@@ -25,9 +25,6 @@ resource "splunk_inputs_script" "script" {
 `
 
 func TestAccSplunkInputsScript(t *testing.T) {
-	if os.Getenv("SKIP_DOCKER") != "" {
-		t.Skip("Skipping not finished test")
-	}
 	// Required since scripted inputs are limited to certain paths within $SPLUNK_HOME
 	path := os.Getenv("SPLUNK_HOME")
 	f := filepath.Join(path, "/bin/scripts/readme.txt")
