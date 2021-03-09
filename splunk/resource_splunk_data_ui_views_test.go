655bb5fc44ec1ed31e627334049d10b128d5cab1
ajayaraman <ajayaraman@splunk.com>
2021-03-09 09:28:37 -0800

    Fix: Data UI Views does not persist permissions on creation

--- splunk/resource_splunk_data_ui_views_test.go
+++ splunk/resource_splunk_data_ui_views_test.go
@@ -10,21 +10,38 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-const splunkDashboardsObject = `
+const splunkDashboardsObjectWithAcl = `
 resource "splunk_data_ui_views" "dashboard" {
-  name     = "Terraform_Test_Dashboard"
+  name     = "terraform"
   eai_data = "<dashboard><label>Terraform Test Dashboard</label></dashboard>"
   acl {
     owner = "admin"
     app = "search"
+    sharing = "global"
+    read = ["*"]
+    write = ["admin"]
   }
+}
+`
+
+const updateSplunkDashboardsObjectWithAcl = `
+resource "splunk_data_ui_views" "dashboard" {
+  name     = "terraform"
+  eai_data = "<dashboard><label>Terraform Test Dashboard</label></dashboard>"
+}
+`
+
+const splunkDashboardsObject = `
+resource "splunk_data_ui_views" "dashboard" {
+  name     = "terraform"
+  eai_data = "<dashboard><label>Terraform Test Dashboard</label></dashboard>"
 }
 `
 
 const updateSplunkDashboardsObject = `
 
 resource "splunk_data_ui_views" "dashboard" {
-  name     = "Terraform_Test_Dashboard"
+  name     = "terraform"
   eai_data = "<dashboard><label>Terraform Test Dashboard</label></dashboard>"
 }
 `
@@ -41,15 +58,63 @@ func TestAccSplunkDashboards(t *testing.T) {
 			{
 				Config: splunkDashboardsObject,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", "Terraform_Test_Dashboard"),
+					resource.TestCheckResourceAttr(resourceName, "name", "terraform"),
 					resource.TestCheckResourceAttr(resourceName, "eai_data", "<dashboard><label>Terraform Test Dashboard</label></dashboard>"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.owner", "admin"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.app", "search"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.sharing", "user"),
 				),
 			},
 			{
 				Config: updateSplunkDashboardsObject,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", "Terraform_Test_Dashboard"),
+					resource.TestCheckResourceAttr(resourceName, "name", "terraform"),
 					resource.TestCheckResourceAttr(resourceName, "eai_data", "<dashboard><label>Terraform Test Dashboard</label></dashboard>"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.owner", "admin"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.app", "search"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.sharing", "user"),
+				),
+			},
+			{
+				ResourceName:      "splunk_data_ui_views.dashboard",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccSplunkDashboardsWithAcl(t *testing.T) {
+	resourceName := "splunk_data_ui_views.dashboard"
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccSplunkDashboardDestroyResources,
+		Steps: []resource.TestStep{
+			{
+				Config: splunkDashboardsObjectWithAcl,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "terraform"),
+					resource.TestCheckResourceAttr(resourceName, "eai_data", "<dashboard><label>Terraform Test Dashboard</label></dashboard>"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.owner", "admin"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.app", "search"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.sharing", "global"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.read.0", "*"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.write.0", "admin"),
+				),
+			},
+			{
+				Config: updateSplunkDashboardsObjectWithAcl,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "terraform"),
+					resource.TestCheckResourceAttr(resourceName, "eai_data", "<dashboard><label>Terraform Test Dashboard</label></dashboard>"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.owner", "admin"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.app", "search"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.sharing", "global"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.read.0", "*"),
+					resource.TestCheckResourceAttr(resourceName, "acl.0.write.0", "admin"),
 				),
 			},
 			{
