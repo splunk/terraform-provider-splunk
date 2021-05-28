6d799428446009a06f643946b94c49aaef5a813f
Micah Kemp <micahk@splunk.com>
2021-05-28 13:44:51 -0500

    Use Defaults for authorization/roles that will have defaults applied by Splunk anyway.  By setting these defaults we can remove "omitempty" for these values, enabling the setting of explicit zero values.

--- client/models/authorization_roles.go
+++ client/models/authorization_roles.go
@@ -14,16 +14,16 @@ type AuthorizationRolesEntry struct {
 
 type AuthorizationRolesObject struct {
 	Capabilities              []string `json:"capabilities,omitempty" url:"capabilities,omitempty"`
-	CumulativeRTSrchJobsQuota int      `json:"cumulativeRTSrchJobsQuota,omitempty" url:"cumulativeRTSrchJobsQuota,omitempty"`
-	CumulativeSrchJobsQuota   int      `json:"cumulativeSrchJobsQuota,omitempty" url:"cumulativeSrchJobsQuota,omitempty"`
+	CumulativeRTSrchJobsQuota int      `json:"cumulativeRTSrchJobsQuota" url:"cumulativeRTSrchJobsQuota"`
+	CumulativeSrchJobsQuota   int      `json:"cumulativeSrchJobsQuota" url:"cumulativeSrchJobsQuota"`
 	DefaultApp                string   `json:"defaultApp,omitempty" url:"defaultApp,omitempty"`
 	ImportedRoles             []string `json:"imported_roles,omitempty" url:"imported_roles,omitempty"`
 	Name                      string   `json:"name,omitempty" url:"name,omitempty"`
-	RtSrchJobsQuota           int      `json:"rtSrchJobsQuota,omitempty" url:"rtSrchJobsQuota,omitempty"`
-	SrchDiskQuota             int      `json:"srchDiskQuota,omitempty" url:"srchDiskQuota,omitempty"`
+	RtSrchJobsQuota           int      `json:"rtSrchJobsQuota" url:"rtSrchJobsQuota"`
+	SrchDiskQuota             int      `json:"srchDiskQuota" url:"srchDiskQuota"`
 	SrchFilter                string   `json:"srchFilter,omitempty" url:"srchFilter,omitempty"`
 	SrchIndexesAllowed        []string `json:"srchIndexesAllowed,omitempty" url:"srchIndexesAllowed,omitempty"`
 	SrchIndexesDefault        []string `json:"srchIndexesDefault,omitempty" url:"srchIndexesDefault,omitempty"`
-	SrchJobsQuota             int      `json:"srchJobsQuota,omitempty" url:"srchJobsQuota,omitempty"`
-	SrchTimeWin               int      `json:"srchTimeWin,omitempty" url:"srchTimeWin,omitempty"`
+	SrchJobsQuota             int      `json:"srchJobsQuota" url:"srchJobsQuota"`
+	SrchTimeWin               int      `json:"srchTimeWin" url:"srchTimeWin"`
 }
