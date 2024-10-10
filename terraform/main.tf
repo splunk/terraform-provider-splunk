777f453d6c0eb2cf21f3cdfe2a8f259cf10c4881
akohring <akohring@stackct.com>
2024-10-10 14:08:36 -0400

    Added splunk app for lookup file editing

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    splunk = {
+      source = "splunk/splunk"
+    }
+  }
+}
+
+provider "splunk" {
+  url      = "localhost:8089"
+  username = "admin"
+  password = "password"
+}
+
+resource "splunk_apps_local" "lookup_file_editing" {
+  filename         = true
+  name             = "/apps/splunk-app-for-lookup-file-editing_404.tgz"
+  explicit_appname = "Splunk App for Lookup File Editing
+}
